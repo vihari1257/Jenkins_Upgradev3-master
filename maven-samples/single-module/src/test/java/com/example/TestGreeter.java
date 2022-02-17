c13a93a973f941722b6bc5edea6f614292f71aef
Vihari Vankadara <vihari1257@gmail.com>
2022-02-18 01:17:38 +0530

    Test push message for Maven

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -40,4 +40,11 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+
+    @Test
+  public void greetShouldIncludeGreetingMessage1() {
+    String someone = "World, Thanks you for Changing";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
 }
